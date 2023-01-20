4b30f91c87d1ffaae5501bea07834d91fbfe5181
DOBOB_LAPTOP <dlehgus1110@naver.com>
2023-01-20 15:38:10 +0900

    feat: cors 설정, body-parser 설정 완료
    email, id값 parsing 확인

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,24 @@
-import express, { Express, Request, Response } from "express";
+import express from 'express'
+import cors from 'cors'
 
-const app: Express = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("start!");
-});
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.post('/login', (req, res) => {
+  console.log('login 시도')
+
+  const successMessage = `
+  <div>
+    <h1>로그인 성공</h1>    
+  </div>
+  `
+  res.send(successMessage)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
