414e7945d367c5a650b18a32bc6cf2d3ef5b8048
Dohyeon Lee <dlehgus1110@naver.com>
2023-01-30 14:24:35 +0900

    config: react vite로 설정 수정

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      <nav className="nav">
+        <a href="/" className="nav_item" data-link>
+          <span>home</span>
+        </a>
+        <a href="/posts" className="nav_item" data-link>
+          <span>login</span>
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
