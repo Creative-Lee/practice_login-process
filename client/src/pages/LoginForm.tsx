5af126b5f7df140c5e399da0c67c7f37f1c30d62
DOBOB_LAPTOP <dlehgus1110@naver.com>
2023-02-03 19:18:43 +0900

    feat: LoginForm 컴포넌트 추가

---
+++ client/src/pages/LoginForm.tsx
@@ -0,0 +1,38 @@
+import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Form onSubmit={onSubmit} className="form__login">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        로그인
+      </Button>
+    </Form>
+  );
+};
+
+export default LoginForm;
